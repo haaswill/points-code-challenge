35c888d84e1e643e4df700ec05702836feb6a349
Willian Haas <haaswillian@gmail.com>
2023-04-27 21:32:11 -0700

    Initial Commit - Initialized NextJS boilerplate - Cleaned up boilerplate - Added Jest - Added Styled-Components

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tax Calculator</title>
+        <meta name="description" content="Tax calculator assignment" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
