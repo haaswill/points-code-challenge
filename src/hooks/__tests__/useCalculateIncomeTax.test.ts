dd27672866c0d7f796a969d055d3f7155769fa52
Willian Haas <haaswillian@gmail.com>
2023-04-30 13:34:36 -0700

    Hook Test - Added test for useCalculateIncomeTax hook

---
+++ src/hooks/__tests__/useCalculateIncomeTax.test.ts
@@ -0,0 +1,22 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { useCalculateIncomeTax } from '../useCalculateIncomeTax';
+import { getTaxBrackets } from '@/__mocks__';
+import { ITaxBracket } from '../../apiConfig/TaxBrackets';
+
+describe('useTaxBrackets', () => {
+  let mockTaxBrackets: ITaxBracket[];
+  const salary = '60000';
+  const correctIncomeTax = '$6,000.00';
+
+  beforeAll(() => {
+    mockTaxBrackets = getTaxBrackets(2021);
+  });
+
+  it('returns the correct income tax', async () => {
+    const { result } = renderHook(() => useCalculateIncomeTax(mockTaxBrackets));
+
+    const incomeTax = result.current.calculateIncomeTax(salary);
+
+    expect(incomeTax).toEqual(correctIncomeTax);
+  });
+});
